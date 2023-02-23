0c24cf3cee2d9e8e385bdca011b80a33dba819da
ttossavainen <teemu.tossavainen@hiq.fi>
2023-02-23 07:07:46 +0200

    init

--- Frends.AmazonS3.DeleteObject/Frends.AmazonS3.DeleteObject/Definitions/Input.cs
+++ Frends.AmazonS3.DeleteObject/Frends.AmazonS3.DeleteObject/Definitions/Input.cs
@@ -33,28 +33,3 @@ public class Input
     /// <example>[ ExampleBucket, ExampleKey, 1 ], [ ExampleBucket, ExampleKey, 'empty' ]</example>
     public S3ObjectArray[] Objects { get; set; }
 }
-
-/// <summary>
-/// Single object.
-/// </summary>
-public class S3ObjectArray
-{
-    /// <summary>
-    /// AWS S3 bucket's name.
-    /// </summary>
-    /// <example>ExampleBucket</example>
-    public string BucketName { get; set; }
-
-    /// <summary>
-    /// The key identifying the object to delete.
-    /// </summary>
-    /// <example>ExampleKey</example>
-    public string Key { get; set; }
-
-    /// <summary>
-    /// VersionId used to reference a specific version of the object.
-    /// Leave empty to use non-versioned delete operation.
-    /// </summary>
-    /// <example>1</example>
-    public string VersionId { get; set; }
-}
---
+++ Frends.AmazonS3.DeleteObject/Frends.AmazonS3.DeleteObject/Definitions/S3ObjectArray.cs
@@ -0,0 +1,26 @@
+﻿namespace Frends.AmazonS3.DeleteObject.Definitions;
+
+/// <summary>
+/// Single object.
+/// </summary>
+public class S3ObjectArray
+{
+    /// <summary>
+    /// AWS S3 bucket's name.
+    /// </summary>
+    /// <example>ExampleBucket</example>
+    public string BucketName { get; set; }
+
+    /// <summary>
+    /// The key identifying the object to delete.
+    /// </summary>
+    /// <example>ExampleKey</example>
+    public string Key { get; set; }
+
+    /// <summary>
+    /// VersionId used to reference a specific version of the object.
+    /// Leave empty to use non-versioned delete operation.
+    /// </summary>
+    /// <example>1</example>
+    public string VersionId { get; set; }
+}
