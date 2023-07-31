c6e5415873acb89f97457115e8bf00409eeb8233
ttossavainen <teemu.tossavainen@hiq.fi>
2023-07-31 10:08:46 +0300

    init

---
+++ Frends.AmazonS3.DeleteBucket/Frends.AmazonS3.DeleteBucket/Definitions/Result.cs
@@ -0,0 +1,26 @@
+﻿namespace Frends.AmazonS3.DeleteBucket.Definitions;
+
+/// <summary>
+/// Task's result.
+/// </summary>
+public class Result
+{
+    /// <summary>
+    /// The operation is complete without errors.
+    /// The operation is considered successful if the bucket to be deleted does not exist. In that case, there will be additional information in Result.Data.
+    /// </summary>
+    /// <example>true</example>
+    public bool Success { get; private set; }
+
+    /// <summary>
+    /// Additional information, such as the 'Bucket to be deleted, does not exist'.
+    /// </summary>
+    /// <example>Bucket to be deleted, does not exist</example>
+    public string Data { get; private set; }
+
+    internal Result(bool success, string data)
+    {
+        Success = success;
+        Data = data;
+    }
+}
